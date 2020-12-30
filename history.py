58f4dfe16c71fe1a310a05564679ba0d9a658f40
gbalissonsilva <alisson.silva@guiabolso.com.br>
2020-12-30 00:13:00 -0300

    add intro

--- history.py
+++ history.py
@@ -1,4 +1,9 @@
-
+from enum import Enum
+class Textos(Enum):
+    intro_guerra = '/home/alissonsilva/scripts/python/objeto/tcc/textos/intro/intro_guerreiro.txt'
+    intro_mago = '/home/alissonsilva/scripts/python/objeto/tcc/textos/intro/intro_mago.txt'
+    intro_besta = '/home/alissonsilva/scripts/python/objeto/tcc/textos/intro/intro_besta.txt'
+    
 class Personagem():
     def __init__(self, nome, tipo):
         self._nome= nome
@@ -14,8 +19,6 @@ class Personagem():
 
     def __str__(self):
         return f' Nome: {self._nome}\n {self._tipo}'    
-
-
 
 class Tipo_personagem():
     def __init__(self):
@@ -34,9 +37,11 @@ class Tipo_personagem():
     def descricao(self):
         raise Exception()
 
+    def add_intro(self):
+        raise Exception()    
 
     def __str__(self):
-        return f'Tipo: {self.add_tipo()} \n Arma: {self.add_arma()} Defesa: {self.add_defesa()} \n Descrição: {self.descricao()}'
+        return f'Tipo: {self.add_tipo()} \n Arma: {self.add_arma()} Defesa: {self.add_defesa()} \n Descrição: {self.descricao()} \n Intro: {self.add_intro()}'
     
 class Guerreiro(Tipo_personagem):
     
@@ -45,6 +50,7 @@ class Guerreiro(Tipo_personagem):
         self._arma = 'Espada'
         self._defesa = 'Escudo'
         
+        
     def add_arma(self):
         return f'{self._arma}' 
 
@@ -52,7 +58,13 @@ class Guerreiro(Tipo_personagem):
         return f'{self._defesa}'
 
     def add_tipo(self):
-        return 'Guerreiiro'        
+        return 'Guerreiiro'
+    
+    def add_intro(self):
+        file = Textos.intro_guerra.value
+        f = open(file, 'r')
+        self._intro = (f.read())
+        return f'{self._intro}'            
 
     def descricao(self):
         return f'Guerreiiro de fé, Nunca Géla!!\n Coragem, força e sangue nos zóio!!'
@@ -76,6 +88,12 @@ class Mago(Tipo_personagem):
     def descricao(self):
         return f'Mostra a cara Mr. M!!\n Vem na magia do Pai!!'
 
+    def add_intro(self):
+        file = Textos.intro_mago.value
+        f = open(file, 'r')
+        self._intro = (f.read())
+        return f'{self._intro}'        
+
 class Besta(Tipo_personagem):
    
     def __init__(self):
@@ -93,7 +111,13 @@ class Besta(Tipo_personagem):
         return 'Besta'        
 
     def descricao(self):
-        return f'Se correr o bicho pega, Se ficar o bicho come'        
+        return f'Se correr o bicho pega, Se ficar o bicho come'
+
+    def add_intro(self):
+        file = Textos.intro_besta.value
+        f = open(file, 'r')
+        self._intro = (f.read())
+        return f'{self._intro}'                
 
 guerreiro = Guerreiro()
 pers1 = Personagem('zé', guerreiro)
